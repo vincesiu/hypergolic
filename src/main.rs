36d566dc2c531fcaede590467ad6cf22c2ffef94
vince <vince.siu4@gmail.com>
2020-03-24 22:27:13 -0700

    create the basic game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+struct Universe {}
+
+fn intialize_universe() -> Universe {
+    return Universe {};
+}
+
+fn process_user_input(universe: &mut Universe) {}
+
+fn update_universe(universe: &mut Universe) {}
+
+fn render(universe: &mut Universe) {
+    println!("{:?}", universe);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // the venerable game loop
+    let mut universe: Universe = intialize_universe();
+    loop {
+        process_user_input(&mut universe);
+        update_universe(&mut universe);
+        render(&mut universe);
+    }
 }
