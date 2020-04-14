4c03e80d4d9dea305bf31982e553806f7b4c29f4
Vincent R. Siu <vincesiu@fb.com>
2020-04-13 19:49:38 -0700

    Add a graphics library to show a blank window

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,47 @@
-#[derive(Debug)]
-struct Universe {}
+use luminance_glfw::{Action, GlfwSurface, Key, Surface as _, WindowDim, WindowEvent, WindowOpt};
+use std::process::exit;
 
-fn intialize_universe() -> Universe {
-    return Universe {};
+struct Universe {
+    surface: GlfwSurface,
 }
 
-fn process_user_input(universe: &mut Universe) {}
+fn intialize_universe() -> Universe {
+    let surface = GlfwSurface::new(
+        WindowDim::Windowed(960, 540),
+        "Hello, world!",
+        WindowOpt::default(),
+    );
+
+    let surface: GlfwSurface = match surface {
+        Ok(surface) => {
+            eprintln!("graphics surface created");
+            surface
+        }
+
+        Err(e) => {
+            eprintln!("cannot create graphics surface:\n{}", e);
+            exit(1);
+        }
+    };
+    return Universe { surface: surface };
+}
+
+fn process_user_input(universe: &mut Universe) {
+    for event in universe.surface.poll_events() {
+        match event {
+            WindowEvent::Close | WindowEvent::Key(Key::Escape, _, Action::Release, _) => exit(0),
+            _ => (),
+        }
+    }
+}
 
 fn update_universe(universe: &mut Universe) {}
 
 fn render(universe: &mut Universe) {
-    println!("{:?}", universe);
+    // rendering code goes here
+
+    // swap buffer chains
+    universe.surface.swap_buffers();
 }
 
 fn main() {
